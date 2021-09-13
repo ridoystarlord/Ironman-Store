24bdf23eb71a51c97077728cb731dd276e800991
Mehedi Hasan Ridoy <ridoy.starlord@gmail.com>
2021-09-13 16:07:49 +0600

    Fix Bugs

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,6 @@
 const loadProducts = () => {
-  //const url = `https://fakestoreapi.com/products`;
-  fetch("../data.json")
+  const url = `https://fakestoreapi.com/products`;
+  fetch(url)
     .then((response) => response.json())
     .then((data) => showProducts(data));
 };
